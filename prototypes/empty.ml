6a3dba05e6d5c68be3582705842633d71eb81299
Matteo Franchin [beta] <franchin@soton.ac.uk>
2010-03-29 16:36:01 +0100

    Updated prototypes for empty files according to recent discussion. Hans, does it look like it should?

--- prototypes/empty.ml
+++ prototypes/empty.ml
@@ -1,6 +1,15 @@
-(* Copyright (C) 2010 University of Southampton
- *  by Name Surname, etc.
+(* Nmag micromagnetic simulator
+ * Copyright (C) 2010 University of Southampton
+ * Hans Fangohr, Thomas Fischbacher, Matteo Franchin and others
  *
- *  Here goes a description of the file.
+ * WEB:     http://nmag.soton.ac.uk 
+ * CONTACT: nmag@soton.ac.uk
+ *
+ * AUTHOR(S) OF THIS FILE: XXX
+ * LICENSE: GNU General Public License 2.0
+ *          (see <http://www.gnu.org/licenses/>)
+ *
+ * Here goes a description of the file XXX.
+ *
  *)
 
