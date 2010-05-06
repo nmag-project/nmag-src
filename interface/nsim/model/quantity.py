689915417eaa7c8021cae307344b962580519543
Matteo Franchin [beta] <franchin@soton.ac.uk>
2010-05-06 14:23:23 +0100

    Introducing a new class Value, used to specify how a field should be set.

--- interface/nsim/model/quantity.py
+++ interface/nsim/model/quantity.py
@@ -24,11 +24,98 @@ a Model, then it is uniquely associated with a field in that model and can
 hence be set, etc.
 """
 
-__all__ = ['Constant', 'SpaceField', 'TimeField', 'SpaceTimeField']
+__all__ = ['Value', 'Constant', 'SpaceField', 'TimeField', 'SpaceTimeField']
 
-import collections
+import collections, types
 from group import Group
 from obj import ModelObj
+
+class Value:
+    """A Value is an object which can be used to set a Quantity.
+    It requires three things: a value, the units and a specification of where
+    the field should be set to that value."""
+
+    def __init__(self, value=None, where=None, unit=None):
+        self.values = []
+        if value != None:
+            self.set(value, where, unit)
+
+    def set(self, value, where=None, unit=None):
+        """An example:
+
+        FieldSetter(1.0).set(0.5, 'Dy').set(0.02, ['Py', 'Fe'])
+
+        sets to 0.5 in the region with name 'Dy', sets to 0.02 in the region
+        'Py' and in the region 'Fe' and sets to 1 in all the remaining
+        regions."""
+        if type(where) == str:
+            where = [where]
+        self.values.append((value, where, unit))
+        return self
+
+    def __repr__(self):
+        fmt = "Value"
+        if len(self.values) == 0:
+            return "%s()" % fmt
+
+        s = ""
+        for value, where, unit in self.values:
+            args = [str(value)]
+            if where != None:
+                if len(where) == 1:
+                    args.append("where='%s'" % where[0])
+                else:
+                    args.append("where=%s" % str(where))
+            if unit != None: args.append("unit=%s" % unit)
+            s += "%s(%s)" % (fmt, ", ".join(args))
+            fmt = ".set"
+        return s
+
+    def as_constant(self, where=None):
+        """Get the value assuming it is constant over the region 'where'.
+        If 'where' is not provided, then assumes the value is constant
+        everywhere in space."""
+        v = None
+        v_default = None
+        if where == None:
+            for value, _, unit in self.values:
+                new_v = (value, unit)
+                if v == None:
+                    v = new_v
+                elif v != new_v:
+                    raise ValueError("as_constant was called on a value "
+                                     "which is not uniform in space: %s."
+                                     % str(self))
+
+        else:
+            if type(where) == str:
+                where = [where]
+
+            for value, my_where, unit in self.values:
+                if my_where == where:
+                    new_v = (value, unit)
+                    if v == None:
+                        v = new_v
+                    elif v != new_v:
+                        raise ValueError("as_constant was called on a value "
+                                         "(%s) which is not uniform in %s."
+                                         % (str(where), self))
+                elif my_where == None:
+                    v_default = (value, unit)
+
+        if v == None:
+            if v_default != None:
+                v = v_default
+            else:
+                raise ValueError("as_constant: value (%s) is not set in %s."
+                                 % (self, str(where)))
+
+        value, unit = v
+        if unit == None:
+            return value
+        else:
+            return value*unit
+
 
 class Quantity(ModelObj):
     """ddd"""
@@ -39,10 +126,15 @@ class Quantity(ModelObj):
                  is_primary=True, def_on_material=False):
         ModelObj.__init__(self, name)
         self.shape = shape
+
         self.value = None
         self.units = units
         self.is_primary = is_primary
         self.def_on_mat = def_on_material
+
+        #self.mwe = None          # MWE associated to the field
+        #self.master = None       # Copy of the field stored on the master node
+
         self.set_value(value)
 
     def is_defined_on_material(self, material):
@@ -81,29 +173,6 @@ class Quantity(ModelObj):
 class Constant(Quantity):
     type_str = "Constant"
 
-    def set_value(self, value, material=None):
-        if self.def_on_mat:
-            if self.value == None:
-                self.value = {}
-            if material == None:
-                self.value = {None: value}
-            else:
-                assert type(material) == str, \
-                  ("Optional argument of Constant.set_value should be either "
-                   "a string or None (the latter to set the constant on "
-                   "every material).")
-                if self.value.has_key(None):
-                    self.value = {material: value}
-                else:
-                    self.value[material] = value
-
-        else:
-            assert material == None, \
-              ("The Constant is not defined per material and hence the "
-               "optional argument material of Constant.set_value should "
-               "not be set to any value other than None.")
-            self.value = value
-
     def is_constant(self):
         return True
 
@@ -119,10 +188,7 @@ class Constant(Quantity):
             v = self.value
 
         else:
-            if self.def_on_mat:
-                v = self.value[material]
-            else:
-                v = self.value
+            v = self.value.as_constant(material)
 
         if in_units:
             return float(v/self.units)
