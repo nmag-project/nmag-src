1448fa4ef01a6c65d6f99e8a2ced35ab3f875567
Matteo Franchin <fnch@users.sourceforge.net>
2011-07-12 12:35:49 +0100

    Adding a function to retrieve a generic mesh with some features. This will be useful in tests (no need to worry about creating a mesh for each individual test).

--- interface/nsim/testtools.py
+++ interface/nsim/testtools.py
@@ -42,3 +42,53 @@ def run_in_dir(directory, command):
   os.chdir(original_dir)
   return exit_status
 
+def get_test_mesh(size=None, dim=None, shape=None, regions=1, periodic=None):
+  """Return the full path to an existing mesh with the required
+  characteristics, or raise an exception if such a mesh doesn't exist.
+
+  :Parameters:
+    `size` : string
+      One of the strings: "small": just few nodes (very small mesh),
+      "medium": mesh big enough to make computation of things like the demag
+      field meaningful, but which still will lead to fast simulations,
+      "big": a mesh which will presumably lead to simulations whose setup
+      phase may take one minute or more.
+
+    `dim` : int
+      The dimension of the mesh (1, 2 or 3).
+
+    `shape` : string
+      The shape of the mesh. Supported shapes are: "ellipsoid".
+
+    `regions` : int
+      Number of regions in the mesh.
+
+    `periodic` : bool
+      Whether the mesh should have periodic points (suitable for PBC
+      simualtions).
+  """
+  test_path_env = "NSIM_TEST_PATH"
+  test_path = os.getenv(test_path_env)
+  if test_path == None:
+    raise StandardError("The environment variable %s is not set."
+                        % test_path_env)
+
+  mesh_path = os.path.join(test_path, "mesh")
+  if not os.path.exists(mesh_path):
+    raise StandardError("The directory '%s' containing the test meshes does "
+                        "not exist:" % mesh_path)
+  size_str = {None: "s", "small": "s", "medium": "m", "large": "l"}[size]
+  shape_str = {None: "ellipsoid", "ellipsoid": "ellipsoid"}[shape]
+  dim_str = {None: "3", 1: "1", 2: "2", 3: "3"}[dim]
+  pbc_str = {None: "n", False: "n", True: "p"}[periodic]
+  nreg_str = {None: "1", 1: "1"}[regions]
+  mesh_filename = (size_str + dim_str + pbc_str + nreg_str + shape_str
+                   + ".nmesh.h5")
+  full_path = os.path.join(mesh_path, mesh_filename)
+
+  if os.path.exists(full_path):
+    return full_path
+  else:
+    raise StandardError("Cannot find a test mesh with the desired features "
+                        "('%s' does not exist)." % full_path)
+
