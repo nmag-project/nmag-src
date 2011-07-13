77b7a47fe8f1e90336d2bf72fea4ce768476bf5d
Matteo Franchin [beta] <franchin@soton.ac.uk>
2011-07-13 18:16:33 +0100

    Updating file.

--- admin/_relbld.bash
+++ admin/_relbld.bash
@@ -1,7 +1,7 @@
 # Automatically generated using distmake-builder 0.1
 # Script generated from the following answers:
 # Q: Do you want to include documentation?
-#    Yes, include documentation.
+#    No, I'm concerned with size.
 # Q: Do you want to include the test suite?
 #    Yes, include the big test suite.
 # Q: Do you want to use the repositories?
@@ -12,21 +12,18 @@
 #    Give me just a tarball.
 # Q: I collected enough information. What should I do:
 #    Generate a bash script to create the distribution file
-. disttools.sh
 # We make the tarball from the current repository rather than the one on the
 # server (gamma.kk)
 REPOS_NSIM_MAIN=`cd .. && pwd`
+. disttools.sh
 PKGNAME=nmag-`get_version ..`
 allsrc_dev_compose "$PKGNAME" 'trunk'
-add_doc "$PKGNAME"
 add_test "$PKGNAME"
 remove_hg "$PKGNAME"
 remove_hg "$PKGNAME/nsim"
 remove_dir "$PKGNAME" nsim/admin nsim/devel nsim/obsolete nsim/prototypes nsim/usersupport nsim/utils
 remove_hg "$PKGNAME/nsim/tests"
-remove_hg "$PKGNAME/doc/manual"
+remove_dir "$PKGNAME" nsim/tests/devtests
 remove_file "$PKGNAME" nsim/bin/nmagprobe
-remove_dir "$PKGNAME" doc/manual/nmag/example_nmagprobe
 gen_tarball "$PKGNAME"
 remove_directory "$PKGNAME"
-
