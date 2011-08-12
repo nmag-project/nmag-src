a3a4f64fd41181cd6c2a6e535eba0fd9d3ea0577
Matteo Franchin <fnch@users.sourceforge.net>
2011-08-12 13:50:47 +0100

    Comment explaining how the Model is built.

--- interface/nsim/model/obj.py
+++ interface/nsim/model/obj.py
@@ -14,6 +14,33 @@ The basic object of a nsim.model.
 The main characteristic of a ModelObj is that it has a type (object.type_str),
 a name (object.name) and a full name which identifies the object univocally
 in the model (call object.get_full_name() to retrieve it).
+
+ModelObj are typically created by the user and are initially detached from
+a model. They are then added to the Model (using 'Model.add_quantity',
+'Model.add_computation' and similar). Once the Model is complete (i.e. all
+objects have been added), a full specification of the mathematical problem
+to be solved is obtained. The 'Model' object does indeed represent such
+a complete description of the problem, a sort of abstract syntax tree of the
+simulation.
+
+The model is then built. When this happens, the Model goes over all its
+objects and obtains their ownership by calling 'ModelObj.own' method.
+The ModelObj can then perform basic operations, those which are only possible
+when the Model is fully specified (simplifying equations and operators,
+generating C code, etc).
+
+Once this phase has finished, the Model goes through the simulation tree and
+computes the dependency tree between all the objects. It identifies the
+primary quantities, how they enter in the computations to give secondary
+quantities. It also understands what computations and quantities are not
+necessary and removes them.
+
+Once this has been done, the Model is finally compiled. This will,
+for example, involve calls to GCC to compile some C code snippets, etc.
+
+Finally, when the model is built, all the objects are 'vivified'.
+For example, the Quantity objects acquire the memory which store their values.
+At this points the objects are ready to be used: the Model is realised.
 '''
 
 class ModelObj(object):
@@ -45,8 +72,18 @@ class ModelObj(object):
            % (self.type_str, self.name))
         return self.lam
 
-    def set_model(self, model):
+    def own(self, model):
+        """Called by the Model to obtain ownership of this object."""
+        #assert self.model == None, "Object was already owned by a Model!"
         self.model = model
+
+    def set_model(self, model):
+        """To be removed."""
+        self.model = model
+
+    def is_owned(self):
+        """Whether the object is owned by another object."""
+        return (self.model != None)
 
     def vivify(self, model):
         """This function is not meant to be called directly by the user.
