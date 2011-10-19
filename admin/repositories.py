353e8abc561f8c89f2adab2e9c5e411f7c1068ec
Matteo Franchin <none@none>
2011-10-19 16:32:29 +0100

    Small fix.

--- admin/repositories.py
+++ admin/repositories.py
@@ -4,5 +4,5 @@ repos_main = repos_path + "/master/src"
 repos_test = repos_path + "/master/test"
 repos_doc  = repos_path + "/master/doc"
 repos_dist = repos_path + "/dist"
-pkgs_file = "/home/hg/local/hg/data/nsim/pkgs.tar"
+pkgs_file = "/home/hg/local/data/nsim/pkgs.tar"
 pkgs_host = host
