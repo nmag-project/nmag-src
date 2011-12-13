402e6ca7b0c8b4363df6168b8409d093e023cda1
Matteo Franchin <none@none>
2011-12-13 15:08:29 +0000

    Committed version 0.2.0

--- interface/nsim/info.py
+++ interface/nsim/info.py
@@ -1,3 +1,4 @@
-version = (0, 1, 0)
-release_date = None
-
+# File generated automatically by ../interface/nsim/version.pyc
+# DO NOT EDIT MANUALLY!
+release_date = 'Tue Dec 13 15:08:29 2011'
+version = (0, 2, 0)
